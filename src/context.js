27632a4cc36072bc022948347d3bbedac2fd2f98
sashasohrabi <shb.sohrabi@gmail.com>
2019-03-07 22:38:08 +0200

    exposing products from contex.js in ProductList.

--- src/context.js
+++ src/context.js
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
-
+import { storeProducts, detailProduct } from "./data";
 const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
+  state = {
+    products: storeProducts,
+    detailProduct: detailProduct
+  };
+
+  handleDetail = () => {
+    console.log("Hello from detail");
+  };
+
+  addToCart = () => {
+    console.log("Hello from add to cart");
+  };
+
   render() {
     return (
-      <ProductContext.Provider value="hello from context">
+      <ProductContext.Provider
+        value={{
+          ...this.state,
+          handleDetail: this.handleDetail,
+          addToCart: this.addToCart
+        }}
+      >
         {this.props.children}
       </ProductContext.Provider>
     );
@@ -14,4 +33,4 @@ class ProductProvider extends Component {
 
 const ProductConsumer = ProductContext.Consumer;
 
-export default { ProductProvider, ProductConsumer };
+export { ProductProvider, ProductConsumer };
