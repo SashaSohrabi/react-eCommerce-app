70d52a5e8acd6e64430a23d10fc0e1235e96166d
sashasohrabi <shb.sohrabi@gmail.com>
2019-03-09 01:01:23 +0200

    added functionality for handleDetail function.

--- src/components/Product.js
+++ src/components/Product.js
@@ -10,32 +10,37 @@ class Product extends Component {
     return (
       <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
         <div className="card">
-          <div
-            className="img-container p-2"
-            onClick={() => console.log("you clicked me!")}
-          >
-            <div className="img-wrapper p-3">
-              <Link to="/details">
-                <img src={img} alt="product" className="card-img-top" />
-              </Link>
-            </div>
-            <button
-              className="cart-btn"
-              disabled={inCart ? true : false}
-              onClick={() => {
-                console.log("added to the cart");
-              }}
-            >
-              {inCart ? (
-                <p className="text-capitalize mb-0" disabled>
-                  {" "}
-                  in cart
-                </p>
-              ) : (
-                <i className="fas fa-cart-plus" />
-              )}
-            </button>
-          </div>
+          <ProductConsumer>
+            {value => (
+              <div
+                className="img-container p-2"
+                onClick={() => value.handleDetail(id)}
+              >
+                <div className="img-wrapper p-3">
+                  <Link to="/details">
+                    <img src={img} alt="product" className="card-img-top" />
+                  </Link>
+                </div>
+                <button
+                  className="cart-btn"
+                  disabled={inCart ? true : false}
+                  onClick={() => {
+                    value.addToCart(id);
+                  }}
+                >
+                  {inCart ? (
+                    <p className="text-capitalize mb-0" disabled>
+                      {" "}
+                      in cart
+                    </p>
+                  ) : (
+                    <i className="fas fa-cart-plus" />
+                  )}
+                </button>
+              </div>
+            )}
+          </ProductConsumer>
+
           {/* card footer*/}
           <div className="card-footer d-flex justify-content-between">
             <p className="align-self-center mb-0">{title}</p>
