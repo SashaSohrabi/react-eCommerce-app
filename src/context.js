baddb50c354cb312164d43e9e8aec1cde2f008d7
sashasohrabi <shb.sohrabi@gmail.com>
2019-03-10 23:59:06 +0200

    added decrement functionality.

--- src/context.js
+++ src/context.js
@@ -93,7 +93,28 @@ class ProductProvider extends Component {
   };
 
   decrement = id => {
-    console.log("this is decrement method");
+    let tempCart = [...this.state.cart];
+    const selectedProduct = tempCart.find(item => item.id === id);
+
+    const index = tempCart.indexOf(selectedProduct);
+    const product = tempCart[index];
+
+    product.count = product.count - 1;
+
+    if (product.count === 0) {
+      this.removeItem(id);
+    } else {
+      product.total = parseFloat((product.count * product.price).toFixed(2));
+
+      this.setState(
+        () => {
+          return { cart: [...tempCart] };
+        },
+        () => {
+          this.addTotals();
+        }
+      );
+    }
   };
 
   removeItem = id => {
